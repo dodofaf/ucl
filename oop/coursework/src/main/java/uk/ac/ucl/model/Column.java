6b72b4d58432de84f422d474e5637449e6f17c4b
dodofaf <makskulysh@gmail.com>
2026-03-10 22:11:20 +0000

    req 1

---
+++ oop/coursework/src/main/java/uk/ac/ucl/model/Column.java
@@ -0,0 +1,33 @@
+package uk.ac.ucl.model;
+
+import java.util.ArrayList;
+
+class Column {
+    private final String name;
+    private final ArrayList<String> data;
+
+    Column (String name) {
+        this.name = name;
+        this.data = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSize() {
+        return data.size();
+    }
+
+    public String getRowValue(int row) {
+        return data.get(row);
+    }
+
+    public void setRowValue(int row, String new_value) {
+        data.set(row, new_value);
+    }
+
+    public void addRowValue(String value) {
+        data.add(value);
+    }
+}
